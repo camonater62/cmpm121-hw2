aedb69c63f22d2eb5bac56c2a7b2e58e3e58e91b
Cameron West <camonater62@gmail.com>
2022-10-17 12:29:29 -0700

    Fix scripted camera speed

--- Assets/panCamera.cs
+++ Assets/panCamera.cs
@@ -14,8 +14,8 @@ public class panCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        totalTime += Time.deltaTime / 3;
-        float dz = Mathf.Cos(totalTime) / 10;
+        totalTime += Time.deltaTime;
+        float dz = Mathf.Cos(totalTime / 3) / 20;
         transform.position += new Vector3(0, 0, dz);
     }
 }
